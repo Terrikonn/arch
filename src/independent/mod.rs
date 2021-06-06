16029436f623b4d6784965da39a04afdcf2a8ff3
remimimimi <valent.xarin@gmail.com>
2021-06-06 14:06:57 +0300

    Add arch independent function for low_power_loop

--- src/independent/mod.rs
+++ src/independent/mod.rs
@@ -1,1 +1,19 @@
 pub mod interrupts;
+
+pub fn low_power_loop() -> ! {
+    cfg_if! {
+        if #[cfg(riscv)] {
+            loop {
+                unsafe {
+                    crate::intrinsics::riscv::asm::wfi();
+                }
+            }
+        } else if #[cfg(x86_64)] {
+            loop {
+                crate::intrinsics::x86_64::instructions::hlt();
+            }
+        } else {
+            unimplemented!();
+        }
+    }
+}
